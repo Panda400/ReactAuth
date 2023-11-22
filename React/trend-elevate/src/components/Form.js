b29d789a7fd4c55dabbb66165b554b0423d736ad
Panda400 <126642107+Panda400@users.noreply.github.com>
2023-11-22 12:10:14 +0000

    Your meaningful commit message here

--- React/trend-elevate/src/components/Form.js
+++ React/trend-elevate/src/components/Form.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import img from '../assets/img1.jpg';
-import apple from '../assets/apple.jpg';
+import apple from '../assets/AppleW.png';
+import google from '../assets/Google.png'
+import windows from '../assets/Windows.png'
+
 
 export default function Form(){
     return(
@@ -10,7 +13,21 @@ export default function Form(){
                 <div className="col-1">
                     <h2>Sign up or login with</h2>
 
-                    <div className='apple-div'> <img src={apple} alt='applelogo'/> </div>
+                    <div className='btn-div'>
+                    <button className='apple-btn'> <img className='appleimg' src={apple} alt='applelogo'/> 
+                    <span className='AppleN'>Apple</span>
+                    </button>
+
+                    <button className='google-btn'> <img className='googleimg' src={google} alt='googlelogo'/>
+                    <span className='GoogleN'>Gmail</span>
+                    </button>
+
+                    <button className='windows-btn'> <img className='windowsimg' src={windows} alt='windowslogo'/>
+                    <span className='WindowsN'>Microsoft</span>
+                    </button>
+
+                    </div>
+
                     <div className='box'>  <span>Sign in to your account</span></div>
 
                     <form id='form' className='flex flex-col'>
